77d22dcb02235535cc24c8083443b69ca755bb80
Luis del Cueto <luisdelcueto@live.com>
2017-06-06 17:23:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0
+
+  function addBump() {
+    counter += 1
+  }
+
+  function getBumps() {
+    return counter
+  }
+
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+function createAnimal(animalType) {
+  // var animal = {}
+    return function (deadlyDevice) {
+      return {
+        animalType,
+        deadlyDevice
+      }
+      // animal.animalType = animalType
+      // animal.deadlyDevice = deadlyDevice
+      // return animal
+  }
+}
+
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+var sharkWithFrickinCannon = sharkCreator('Cannon');
